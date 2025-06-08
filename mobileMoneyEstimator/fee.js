2e5f9c22b8e8ec5eabf3f4f5a340f0d9415e4b41
peter munyambu <petermunyambu1000@gmail.com>
2025-06-08 22:12:20 +0300

    update fee.js

--- mobileMoneyEstimator/fee.js
+++ mobileMoneyEstimator/fee.js
@@ -2,3 +2,9 @@ function createTransactionFeeEstimator() {
   const minFee = 10;
   const maxFee = 70;
   const feeRate = 0.015;
+return function estimateTransactionFee(amountToSend) {
+    if (isNaN(amountToSend) || amountToSend <= 0) {
+      console.log("unatuma ngapi?");
+      return;
+    }
+
