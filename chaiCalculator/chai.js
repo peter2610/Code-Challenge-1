3f1bee6bb4c1f10c070aaf481b3fd5a7f000a3fc
peter munyambu <petermunyambu1000@gmail.com>
2025-06-08 21:35:43 +0300

    git chaicalculator.js function

---
+++ chaiCalculator/chai.js
@@ -0,0 +1,7 @@
+function chaiCalculator(numberOfCups) {
+  const water = 200 * numberOfCups;         // ml
+  const milk = 50 * numberOfCups;           // ml
+  const teaLeaves = 1 * numberOfCups;       // tablespoons
+  const sugar = 2 * numberOfCups;           // teaspoons
+
+  
